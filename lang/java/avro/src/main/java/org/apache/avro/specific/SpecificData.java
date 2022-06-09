78b89df51aa793dbfc67109df4791afa7cec8fce
Ashley Taylor <ashley.taylor@phocassoftware.com>
2022-06-09 12:37:50 +1200

    spotless

--- lang/java/avro/src/main/java/org/apache/avro/specific/SpecificData.java
+++ lang/java/avro/src/main/java/org/apache/avro/specific/SpecificData.java
@@ -664,6 +664,7 @@ public class SpecificData extends GenericData {
       return customEncoding;
     }
   }
+
   /** Needed for simulating schema change during test. */
   protected void removeFromSchemaClassCache(Class<?> c) {
     schemaClassCache.remove(c);
